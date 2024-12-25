92dcc3adee8e7e3967780db2142939ba9cbe13e9
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-25 17:16:26 +0530

    added post component and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Posts from './components/posts/Posts';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='distantgalaxy text-6xl  font-mono '>
+      <h1 className='distantgalaxy text-6xl  font-mono text-center '>
         Sachin's Blogs  
       </h1>
-      <h2 className='battlestar text-3xl font-thin text-sky-500 '>
+      <h2 className='battlestar text-3xl font-thin text-sky-500 text-center '>
         <span> Made with React, FireBase and LightSabers</span>
       </h2>
       {/* <img className='w-12' src="src\assets\images\lightsaber_blue.gif" alt="" /> */}
+      <Posts />
     </>
   )
 }
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import Posts from './Posts';
 
 function Posts() {
   return (
-    <div>
-        Recent Posts
-        <h1 className='2xl text-gray-500'>
-            Star wars Skeleton Crew Review
-        </h1>
-        <div className=' text-slate-200'>
-            It has the potential to be a kids series set in 
-            Star Wars universe. But derives lots of overused
-            and generic concepts in hollywood storytelling.
+    
+    <div className=' '>
+      <h1 className='text-2xl ml-20  my-3 rebellion '>
+          Recent Posts
+      </h1>   
+        
+        <div className=' w-2/3 flex bg-slate-800 p-6 mx-auto flex-col justify-center'>
+           
+           <img className='w-2/3 self-center  my-1 bg-slate-400 p-2 rounded-md' src="https://upload.wikimedia.org/wikipedia/commons/9/92/Skeleton_Crew.svg" alt="" />
+           
+           
+           <div className=' flex flex-col justify-center '>
+
+              <h2 className='text-3xl font-mono font-bold text-green-400'>
+                  Review : Star wars skeleton crew
+                </h2>
+              <p className=' poppins  text-slate-300 font-serif'> 
+                It has the potential to be a kids series set in 
+                Star wars universe. But derives lots of overused
+                and generic concepts in hollywood storytelling.
+                </p>
+                <a href='' className='battlestar text-center'>
+                  Read Full Review ↓
+                </a>
+            </div>
         </div>
 
 
